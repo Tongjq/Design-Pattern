b8943c03e29238148f94e26d8a99088b59f98fcd
JunqiangOwner <tjqadmin@163.com>
2021-01-06 17:26:54 +0800

    工厂方法

---
+++ P5_FactoryMethodPattern/FactoryMethodPatternDemo.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P5_FactoryMethodPattern
+{
+    class FactoryMethodPatternDemo
+    {
+        public void Test()
+        {
+            IFactory operFactory = new AddFactory();
+            Operation oper = operFactory.CreateOperation();
+
+            oper.NumberA = 1;
+            oper.NumberB = 2;
+
+            double reautlt= oper.GetResult();
+        }
+    }
+
+
+    interface IFactory
+    {
+        Operation CreateOperation();
+    }
+
+    class AddFactory : IFactory
+    {
+        public  Operation CreateOperation()
+        {
+            return new OperationAdd();
+        }
+    }
+
+    class SubFactrory : IFactory
+    {
+        public Operation CreateOperation()
+        {
+            return new OperationSub();
+        }
+    }
+
+
+    public class Operation
+    {
+        private double _numberA;
+        private double _numberB;
+
+        public double NumberA
+        {
+            set { _numberA = value; }
+
+            get { return _numberA; }
+        }
+
+        public double NumberB
+        {
+            set { _numberB = value; }
+
+            get { return _numberB; }
+
+        }
+
+        public virtual double GetResult()
+        {
+            double result = 0;
+            return result;
+        }
+
+    }
+
+    public class OperationAdd : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            result = NumberA + NumberB;
+
+            return result;
+        }
+    }
+
+    public class OperationSub : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            result = NumberA - NumberB;
+
+            return result;
+        }
+    }
+
+}
---
+++ P5_FactoryMethodPattern/XueLeiFeng.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P5_FactoryMethodPattern
+{
+    class XueLeiFeng
+    {
+        public void Test()
+        {
+            // 简单工厂
+            LeiFeng studentA=  SimpleFactory.CreateLeiFeng("学雷锋的大学生");
+            LeiFeng studentB = SimpleFactory.CreateLeiFeng("学雷锋的大学生");
+            LeiFeng studentC = SimpleFactory.CreateLeiFeng("学雷锋的大学生");
+
+            studentA.BuyRise();
+            studentB.Wash();
+            studentC.Sweep();
+
+            // 工厂方法
+            ILeiFengFactory factory =/* new UndergraduateFactory();*/ new VolunteerFactory();
+            LeiFeng student = factory.CreateLeiFeng();
+            student.Sweep();
+            student.Wash();
+            student.BuyRise();
+
+
+
+
+        }
+        
+    }
+
+
+    /// <summary>
+    /// 雷锋
+    /// </summary>
+    class LeiFeng
+    {
+        public void Sweep()
+        {
+            Console.WriteLine("扫地");
+        }
+
+        public void Wash()
+        {
+            Console.WriteLine("洗衣");
+        }
+
+        public void BuyRise()
+        {
+            Console.WriteLine("买米");
+        }
+    }
+
+    /// <summary>
+    /// 学雷锋的大学生
+    /// </summary>
+    class Undergraduate : LeiFeng
+    { }
+
+    /// <summary>
+    /// 社区志愿者
+    /// </summary>
+    class Volunteer : LeiFeng
+    { }
+
+    #region 简单工厂
+    /// <summary>
+    /// 简单工厂
+    /// </summary>
+    class SimpleFactory
+    {
+        public static LeiFeng CreateLeiFeng(string type)
+        {
+            LeiFeng result = null;
+            switch (type)
+            {
+                case "学雷锋的大学生":
+                    result = new Undergraduate();
+                    break;
+                case "社区志愿者":
+                    result = new Volunteer();
+                    break;
+            }
+
+            return result;
+        }
+    }
+    #endregion
+
+    #region 工厂方法
+    /// <summary>
+    /// 雷锋工厂
+    /// </summary>
+    interface ILeiFengFactory
+    {
+        LeiFeng CreateLeiFeng();
+    }
+
+    /// <summary>
+    /// 学雷锋的大学生工厂
+    /// </summary>
+    class UndergraduateFactory : ILeiFengFactory
+    {
+        public LeiFeng CreateLeiFeng()
+        {
+            return new Undergraduate();
+        }
+    }
+
+    /// <summary>
+    /// 社区志愿者工厂
+    /// </summary>
+    class VolunteerFactory : ILeiFengFactory
+    {
+        public LeiFeng CreateLeiFeng()
+        {
+            return new Volunteer();
+        }
+    } 
+    #endregion
+
+}
